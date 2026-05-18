8ebbdcba6a7760010682736434784c92b88c3e72
mskayla785 <mskayla785@gmail.com>
2026-05-18 18:58:35 +0700

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ st.set_page_config(page_title="Multiple Disease Prediction", layout="wide", page
 #heart_disease_model = pickle.load(open(f'{working_dir}/saved_models/heart.pkl', 'rb'))
 #kidney_disease_model = pickle.load(open(f'{working_dir}/saved_models/kidney.pkl', 'rb'))
 
+path = 'https://github.com/mskayla785/dheartclover/tree/main/'
 diabetes_model = pickle.load(open(path + 'saved_models/diabetes.pkl', 'rb'))
 heart_disease_model = pickle.load(open(path + 'saved_models/heart.pkl', 'rb'))
 kidney_disease_model = pickle.load(open(path + 'saved_models/kidney.pkl', 'rb'))
